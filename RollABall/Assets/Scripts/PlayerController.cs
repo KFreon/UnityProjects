e7ef9c556b512b93b2c7c8d4c3d2be6e5b6dd506
Kael Larkin <kael.larkin@outlook.com>
2020-05-26 20:05:13 +1000

    Finished tutorial

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float ForceMultiplier;
+    public Text ScoreText;
+    public Text WinText;
+
+    private Rigidbody PlayerObject;
+
+    private int score = 0;
+
+    private void Start()
+    {
+        PlayerObject = GetComponent<Rigidbody>();
+        SetScoreText(score);
+        WinText.text = "";
+    }
+
+    // Before frame
+    private void Update()
+    {
+        
+    }
+
+    // Before physics calcs
+    private void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        PlayerObject.AddForce(movement * ForceMultiplier);
+    }
+
+    // Detecs collision between THIS object and "other"
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            SetScoreText(++score);
+        }
+    }
+
+    private void SetScoreText(int score)
+    {
+        ScoreText.text = $"Score: {score}";
+
+        if (score > 5)
+        {
+            WinText.text = "Winnaaaaa!!";
+        }
+    }
+}
